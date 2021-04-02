e549e510221237ce73e71844d629cade9aabd37d
OwenOu <owenou@owendeMac-mini.local>
2021-04-02 14:51:27 -0400

    finished aperature

--- src/student/camera.cpp
+++ src/student/camera.cpp
@@ -14,19 +14,28 @@ Ray Camera::generate_ray(Vec2 screen_coord) const {
     screen_coord = screen_coord - Vec2(0.5f); // make center the origin
     
     // get screen height and screen width
-    float sh = 2.0f * tan((vert_fov / 2.0f) / 180.0f * M_PI);
+    float sh = 2.0f * focal_dist * tan((vert_fov / 2.0f) / 180.0f * M_PI);
     float sw = sh * aspect_ratio;
         
     // scale coordinate
     screen_coord = screen_coord * Vec2(sw, sh);
         
+    // start position with aperature
+    Vec2 apertureSize(aperture);
+    Samplers::Rect::Uniform sampler(apertureSize);
+    float pdf;
+    Vec2 aperturePos = sampler.sample(pdf) - Vec2((aperture / 2.0f)); // centered on origin
+    
     // world coordinate, homogeneous
-    // 1 unit negative away from camera
-    Vec3 world_coord(screen_coord.x, screen_coord.y, -1.0f);
+    // focal distance away from camera
+    Vec3 world_coord(screen_coord.x, screen_coord.y, -1.0f * focal_dist);
+    Vec3 aperture_world(aperturePos.x, aperturePos.y, 0.0f);
+    
     world_coord = iview * world_coord;
-        
-    Vec3 dir = world_coord - position;
+    aperture_world = iview * aperture_world;
+    
+    Vec3 dir = world_coord - aperture_world;
     
     // output ray from camera, point to screen point
-    return Ray(position, dir);
+    return Ray(aperture_world, dir);
 }
