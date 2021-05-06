63e7ac6632dd83132f88c19ae93f3975df8fd18d
OwenOu <owenou@owendeMac-mini.local>
2021-05-05 23:53:50 -0400

    finished task 4

--- src/student/particles.cpp
+++ src/student/particles.cpp
@@ -11,7 +11,7 @@ bool Particle::update(const PT::BVH<PT::Object>& scene, float dt, float radius) 
     
     // build ray on particle
     Ray trajectory(pos, velocity);          // velocity turn to unit vector automatically
-    Vec3 particleDir = velocity.unit();     // direction of partical motion
+    Vec3 particleDir = velocity.unit();     // direction of partical motion, unit vector
     float dist = velocity.norm() * dt;      // travel distance in dt
 
     // TODO: handle close collision
@@ -19,39 +19,47 @@ bool Particle::update(const PT::BVH<PT::Object>& scene, float dt, float radius) 
     
     PT::Trace hit = scene.hit(trajectory);      // find hit point
     
-    if(hit.hit){
-        // if hit, check bounce
-        // TODO: account for particle radius, ray hit point is not actual collide point
-        // sine of angle between partical ray to surface normal
-        float cos_theta = dot(-1.0f * particleDir, hit.normal);
-        float dist = hit.distance - radius / cos_theta;   // dist to center of particle when hit
-        
-        float ddt = dist / velocity.norm();           // actual time for hit
-        
-        if(dist > 0){
-            // particle center when hit, move to hit point
-            pos += dist * particleDir;
-            // same magnitude, direction reflected
-            velocity = velocity - 2.0f * dot(velocity, hit.normal) * hit.normal + acceleration * ddt;
-            
-        }else{
-            // if next hit point smaller than radius, immediate reflect
-            pos += (velocity - 2.0f * dot(velocity, hit.normal) * hit.normal) * ddt;
-            velocity = (velocity - 2.0f * dot(velocity, hit.normal) * hit.normal) + acceleration * ddt;
-        }
-        
-        // TODO: handle multiple hits in same time step
-        age -= ddt;
-        update(scene, dt - ddt, radius);    // recursive update for multiple hit in same time step
-        
-    }else{
+    if(!hit.hit){
         // if not hit, update base on pos and velocity
         pos += velocity * dt;
         velocity += acceleration * dt;
-        
         // substract age
         age -= dt;
+        return age > 0.0f;
     }
+    
+    
+    float cos_theta;
+    float d;           // center position when radius hit surface
+    float ddt = 0.0f;
+    
+    while(hit.hit && ddt < dt){
         
-    return age > 0;
+        cos_theta = dot((-1.0f * particleDir), hit.normal);
+        d = radius / abs(cos_theta);    // make sure all positive
+        
+        if(hit.distance <= d + d / 10.0f){
+            // partical already at surface, reflect immediately
+            // position not changed
+            velocity = velocity - 2.0f * dot(velocity, hit.normal) * hit.normal; // reflect against normal
+                        
+        }else{
+            // if partical not yet reach surface
+            
+            float timeSpent = (hit.distance - d) / velocity.norm();             // find actual time spend to hit surface
+            pos += velocity * timeSpent;                                        // move to contact position
+            velocity += acceleration * timeSpent;
+            
+            ddt += timeSpent;
+        }
+        
+        trajectory = Ray(pos, velocity);
+        dist = velocity.norm() * (dt - ddt);
+        trajectory.dist_bounds = Vec2(0.0f, dist);
+        hit = scene.hit(trajectory);
+        
+    }
+    
+    age -= dt;
+    return age > 0.0f;
 }
